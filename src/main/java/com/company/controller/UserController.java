2abd4ff416dbf2044f805b7a27046306c310dd38
Ümüt Günaydın <umutgunaydin65@gmail.com>
2023-05-08 23:26:01 +0300

    updating user done

--- src/main/java/com/company/controller/UserController.java
+++ src/main/java/com/company/controller/UserController.java
@@ -45,17 +45,17 @@ public class UserController {
 
         return "redirect:/user/create";
     }
-//
-//    @GetMapping("/update/{username}")
-//    public String editUser(@PathVariable("username") String username, Model model){
-//
-//        model.addAttribute("user", userService.findById(username));
-//        model.addAttribute("roles",roleService.findAll());
-//        model.addAttribute("users",userService.findAll());
-//
-//
-//        return "/user/update";
-//    }
+
+    @GetMapping("/update/{username}")
+    public String editUser(@PathVariable("username") String username, Model model){
+
+        model.addAttribute("user", userService.findByUserName(username));
+        model.addAttribute("roles",roleService.listAllRoles());
+        model.addAttribute("users",userService.listAllUsers());
+
+
+        return "/user/update";
+    }
 //
 //    @PostMapping("/update")
 //    public String updateUser(@Valid @ModelAttribute("user") UserDTO user){ // without using @ModelAttribute it is ok
--- src/main/java/com/company/service/impl/UserServiceImpl.java
+++ src/main/java/com/company/service/impl/UserServiceImpl.java
@@ -33,7 +33,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDTO findByUserName(String username) {
-        return null;
+        return userMapper.convertToDto(userRepository.findByUserName(username));
     }
 
     @Override
