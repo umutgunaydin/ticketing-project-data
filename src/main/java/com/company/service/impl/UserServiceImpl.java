6f29e7aeb93b8d8b9e87edfaa6b7fd38f5ddb03f
Ümüt Günaydın <umutgunaydin65@gmail.com>
2023-05-08 23:17:15 +0300

    saving user to db done

--- src/main/java/com/company/service/impl/UserServiceImpl.java
+++ src/main/java/com/company/service/impl/UserServiceImpl.java
@@ -2,27 +2,33 @@ package com.company.service.impl;
 
 import com.company.dto.UserDTO;
 import com.company.entity.User;
+import com.company.mapper.UserMapper;
 import com.company.repository.UserRepository;
 import com.company.service.UserService;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
         this.userRepository = userRepository;
+        this.userMapper = userMapper;
     }
 
     @Override
     public List<UserDTO> listAllUsers() {
 
         List<User> userList=userRepository.findAll(Sort.by("firstName"));
-
-        return null;
+        List<UserDTO> userDTOList=userList.stream().map(userMapper::convertToDto).collect(Collectors.toList());
+        return userDTOList
+                ;
     }
 
     @Override
@@ -32,7 +38,8 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void save(UserDTO user) {
-
+        User entity=userMapper.convertToEntity(user);
+        userRepository.save(entity);
     }
 
     @Override
