458291bc4f8798244d3b2488aa471e7d14770b5a
Ümüt Günaydın <umutgunaydin65@gmail.com>
2023-05-10 12:33:06 +0300

    updating/inserting user by infos done

--- src/main/java/com/company/entity/BaseEntity.java
+++ src/main/java/com/company/entity/BaseEntity.java
@@ -17,9 +17,31 @@ public class BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Boolean isDeleted = false;
+    @Column(nullable = false,updatable = false)
+
     private LocalDateTime insertDateTime;
+    @Column(nullable = false,updatable = false)
+
     private Long insertUserId;
+    @Column(nullable = false)
+
     private LocalDateTime lastUpdateDateTime;
+    @Column(nullable = false)
+
     private Long lastUpdateUserId;
+
+    @PrePersist
+    private void onPrePersist(){
+        this.insertDateTime=LocalDateTime.now();
+        this.lastUpdateDateTime=LocalDateTime.now();
+        this.insertUserId=1L;
+        this.lastUpdateUserId=1L;
+    }
+    @PreUpdate
+    private void onPreUpdate(){
+        this.lastUpdateDateTime=LocalDateTime.now();
+        this.lastUpdateUserId=1L;
+    }
+
 
 }
